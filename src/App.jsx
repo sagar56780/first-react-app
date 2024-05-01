eb31898697876030b64d1b152aefa0e335382d39
sagar kumar <81793637+sagar56780@users.noreply.github.com>
2024-05-01 12:28:54 +0530

    using component multiple time and passing diffrent value to each component

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,16 @@ class App extends React.Component{
 */
 //!============Props===============
 import ChildCompnent from "./ChildComponent";
-const App=()=>{     
+const App=()=>{    //sending props - props is addition property passing to a element like we pass values inside input tag in html
     return(
-         <ChildCompnent image="FirstImage" discription="info1" price="100" discount="90%"/>  // sending props 
+        <>
+         <ChildCompnent image="FirstImage" discription="info1" price="100" discount="90%"/> 
+         <ChildCompnent image="secondImage" discription="info2" price="500" discount="70%"/>  
+         <ChildCompnent image="thirdImage" discription="info3" price="1000" discount="39%"/> 
+         <ChildCompnent image="fourthImage" discription="info4" price="1100" discount="50%"/> 
+         <ChildCompnent image="fithImage" discription="info5" price="600" discount="80%"/>  
 
+         </>
     );
 }
 export default App;
--- src/ChildComponent.jsx
+++ src/ChildComponent.jsx
@@ -7,7 +7,10 @@ const ChildCompnent=(props)=>{
         <h1>Description:{props.discription}</h1>
         <h1>Price{props.price}</h1>
         <h1>Discount{props.discount}</h1>
-        </>
+
+        <hr /> 
+             </>
+        
     );
 }
 export default ChildCompnent;
